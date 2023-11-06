ff368b8eca49a0d0adc52dd287202a3b526440fd
Antoine Laurent <alaurent@agencemobilitedurable.ca>
2023-11-06 10:34:06 -0500

    feat: make computation count freq user defined

--- montinette/sensors.py
+++ montinette/sensors.py
@@ -1,8 +1,11 @@
+from functools import reduce
 import pandas as pd
 from pandas.util._validators import validate_bool_kwarg
 import numpy as np
 
 from .constants import PROJECT_TIMEZONE, COUNTER_COL, DATETIME_COL, UUID_COL
+from .utils import reset_count_on_interval
+
 
 
 class CounterSeries(pd.Series):
@@ -52,7 +55,7 @@ class CounterDataFrame(pd.DataFrame):
             raise TypeError('DataFrame constructor called with incompatible data and dtype: {e}'.format(e=type(data)))
 
         kwargs.pop('columns', None)
-        super().__init__(cdf, columns=columns, **kwargs)
+        super(CounterDataFrame, self).__init__(cdf, columns=columns, **kwargs)
 
         if self._has_counter_columns():
             self._set_counter(timestamp=timestamp, timezone=timezone, inplace=True)
@@ -72,6 +75,7 @@ class CounterDataFrame(pd.DataFrame):
         data = data[~data[COUNTER_COL].isna()]
         data = data.sort_values(DATETIME_COL)
         data['reset'] = ((data[COUNTER_COL].diff() < 0) & (data[COUNTER_COL] <= 10)).cumsum()
+
 
         if data['reset'].value_counts().shape[0] > 1:
             print('Resets in counts have been detected. Column "reset" created.')
@@ -96,19 +100,15 @@ class CounterDataFrame(pd.DataFrame):
 
     def _clean(self, inplace=False):
         inplace = validate_bool_kwarg(inplace, 'inplace')
+
         if not inplace:
             data = self.copy()
-        else:
-            data = self
-
-        #data = data.dropna()
-
-        if not inplace:
             data = data._detect_reset_count()._detect_count_reduction()
             return data
 
-        data._detect_reset_count(inplace=inplace)
-        data._detect_count_reduction(inplace=inplace)
+        else:
+            self._detect_reset_count(inplace=inplace)
+            self._detect_count_reduction(inplace=inplace)
 
     def _delta(self, clean=True, inplace=False, reset_interval=0):
         inplace = validate_bool_kwarg(inplace, 'inplace')
@@ -118,24 +118,13 @@ class CounterDataFrame(pd.DataFrame):
             data = self
 
         if clean:
-            data = data._clean()
+            data = data._clean(inplace=inplace)
 
         if 'count_delta' in data.columns:
             data.rename(columns={'count_delta':'user_delta_cp'}, inplace=True)
 
-        #TODO : shoot in utils, useless here. Needed while cumsum
         # day interval
-        data['day_interval'] = ((data[DATETIME_COL].dt.dayofyear +
-                                   data[DATETIME_COL].dt.dayofyear.iloc[0]) //
-                                   reset_interval -
-                                   data[DATETIME_COL].dt.dayofyear.iloc[0] // reset_interval) if reset_interval > 0 else 0
-        data['year'] = data.timestamp.dt.year
-        data['reset_interval'] = data.year.astype(str) + ' ' + data.day_interval.astype(str)
-        data = data.assign(
-            reset_interval=data.reset_interval.replace({data.reset_interval.unique()[i]:i+1
-                                                        for i in range(len(data.reset_interval.unique()))})
-        )
-        data.drop(columns=['day_interval', 'year'], inplace=True)
+        data = reset_count_on_interval(data, interval=reset_interval, time_col=DATETIME_COL)
 
         cpt_in_lst = []
         for _, data_r in data.groupby(['reset', 'reset_interval']):
@@ -152,6 +141,26 @@ class CounterDataFrame(pd.DataFrame):
         if not inplace:
             return data
 
+    def get_counts(self, freq='15T', clean=True, reset_interval=0):
+        """
+        Paramaters
+        ----------
+        freq: str (Default: '15T')
+            The offset string or object representing target conversion (see
+            https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#dateoffset-objects).
+        clean: bool (Default: True)
+            Perform a cleaning of the data before analysing it.
+        reset_interval: int (Default: 0)
+            Interval in day from which to reset the counter.
+        """
+        if 'count_delta' not in self.columns:
+            data = self._delta(clean=clean, inplace=False, reset_interval=reset_interval).copy()
+
+        result = data.set_index(DATETIME_COL).resample(freq)['count_delta'].sum().to_frame('count_delta')
+
+        return result
+
+
     def get_hour_counts(self, clean=True, reset_interval=0):
         """
         Paramaters
@@ -161,18 +170,16 @@ class CounterDataFrame(pd.DataFrame):
         reset_interval: int (Default: 0)
             Interval in day from which to reset the counter.
         """
-        data = self.copy()
 
-        if 'count_delta' not in data.columns:
-            data = data._delta(clean=clean, reset_interval=reset_interval)
-
-        result = data.groupby([data[DATETIME_COL].dt.date, data[DATETIME_COL].dt.hour])['count_delta'].sum().to_frame('count_delta')
-        result.index.names = ['date', 'hr']
-
+        result = self.get_counts('1H', clean=clean, reset_interval=reset_interval)
         result = result.reset_index()
-        result['date'] = pd.to_datetime(result['date'])
 
+        result['date'] = result[DATETIME_COL].dt.date
+        result['date'] = pd.to_datetime(result['date'])
+        result['hr'] = result[DATETIME_COL].dt.hour
         result = result.set_index(['date', 'hr'])
+
+        result = result.drop(columns=DATETIME_COL)
 
         return result
 
@@ -213,13 +220,25 @@ class CounterDataFrame(pd.DataFrame):
 
     def _is_counterframe(self):
 
-        if ((COUNTER_COL in self) and
-                (pd.core.dtypes.common.is_float_dtype(self[COUNTER_COL]) or
-                 pd.core.dtypes.common.is_integer_dtype(self[COUNTER_COL]))) \
-            and ((DATETIME_COL in self) and
-                 pd.core.dtypes.common.is_datetime64_any_dtype(self[DATETIME_COL])) \
-            and ((UUID_COL in self) and
-                (pd.core.dtypes.common.is_string_dtype(self[UUID_COL]))):
+        check_counter_col = (
+            (COUNTER_COL in self) and
+            (
+                pd.core.dtypes.common.is_float_dtype(self[COUNTER_COL]) or
+                pd.core.dtypes.common.is_integer_dtype(self[COUNTER_COL])
+            )
+        )
+
+        check_datetime_col = (
+            (DATETIME_COL in self) and
+            pd.core.dtypes.common.is_datetime64_any_dtype(self[DATETIME_COL])
+        )
+
+        check_uuid_col = (
+        (UUID_COL in self) and
+                (pd.core.dtypes.common.is_string_dtype(self[UUID_COL]))
+        )
+
+        if check_counter_col and  check_datetime_col and check_uuid_col:
             return True
 
         return False
@@ -236,6 +255,10 @@ class CounterDataFrame(pd.DataFrame):
 
         elif isinstance(result, CounterDataFrame) and not result._is_counterframe():
             result.__class__ = pd.DataFrame
+
+        elif isinstance(result, pd.DataFrame) and result._is_counterframe():
+            result.__class__ = CounterDataFrame
+
         return result
 
     @property
@@ -267,3 +290,34 @@ class CounterDataFrame(pd.DataFrame):
                 object.__setattr__(self, name, getattr(other, name, None))
 
         return self
+
+def merge_list_df(list_df):
+
+    df = reduce(lambda df1,df2: df1.join(df2, how='inner', rsuffix='_X'), list_df)
+    df['count_delta'] = df.sum(axis=1)
+    return df[['count_delta']]
+
+def count_veh_in_spot(cpt_in:CounterDataFrame | list, cpt_out:CounterDataFrame, reset_interval:int = 0):
+    if isinstance(cpt_in, list):
+        cpt_in = merge_list_df(cpt_in)
+    if isinstance(cpt_out, list):
+        cpt_out = merge_list_df(cpt_out)
+
+    cpt_in = cpt_in.copy()
+    cpt_out = cpt_out.copy()
+    
+    cpt_in['in'] = cpt_in['count_delta']
+    cpt_out['out'] = cpt_out['count_delta']
+
+    # join in and out
+    data = cpt_in.join(cpt_out, how='inner', lsuffix='_in')[['in', 'out']]
+
+    # reset_interval
+    data = data.reset_index()
+    data = reset_count_on_interval(data, interval=reset_interval, time_col='timestamp')
+    
+    # compute delta and parked veh
+    data['delta'] = data['in'] - data['out']
+    data['nb_veh'] = data.groupby('reset_interval')['delta'].cumsum()
+
+    return data
